a52184c852cac29f0b6c3a0917140178a5915873
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-12-16 22:06:39 +0000

    Modify Postgresql port

--- cara/config/test.exs
+++ cara/config/test.exs
@@ -9,6 +9,7 @@ config :cara, Cara.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  port: 15432,
   database: "cara_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
