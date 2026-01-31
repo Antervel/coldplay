28a1aabea5d122c83f429b197a544051a3279c86
Diego M. Vadell <diego.m.vadell@gmail.com>
2026-01-31 23:02:34 +0000

    Fix scrolling

--- cara/assets/js/app.js
+++ cara/assets/js/app.js
@@ -29,13 +29,23 @@ const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute
 let Hooks = {}
 Hooks.ChatScroll = {
   mounted() {
+    this.isAtBottom = true // Assume at bottom initially
+    this.el.addEventListener("scroll", () => {
+      // Use a small tolerance for "at bottom" check due to potential floating point inaccuracies
+      this.isAtBottom = this.el.scrollHeight - this.el.clientHeight - this.el.scrollTop < 1
+    })
     this.scrollToBottom()
   },
   updated() {
-    this.scrollToBottom()
+    // Check if we *were* at the bottom before the update
+    if (this.isAtBottom) {
+      this.scrollToBottom()
+    }
   },
   scrollToBottom() {
     this.el.scrollTop = this.el.scrollHeight
+    // After scrolling, we are definitely at the bottom
+    this.isAtBottom = true
   }
 }
 
